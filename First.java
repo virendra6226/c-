e84d741a8c24bfff35309a1afab6f31c154ce7d4
Virendra Kumar Dwivedi <dwivedivirendrakumar23@gmail.com>
2025-10-28 21:41:38 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,18 @@
+class First{
+   static void fun()
+    {
+        System.out.println("call f1 function");
+    }
+    static void fun2()
+    {
+        System.out.println("call f2 function");
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello world");
+        fun();
+        fun2();
+    }
+
+}
